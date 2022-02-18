9f4c407bd37cae825ba7a326f3f5e6bbefbd12cc
Ashu Mitter <ashu976sincere@gmail.com>
2022-02-18 12:56:07 +0530

    commit 23

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MyFirstComponent from './myfirstcomponent';
+
+
+
+
+ReactDOM.render(<MyFirstComponent/>,
+    document.getElementById('root'));
---
+++ myfirstcomponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+var val = <p>Hello World</p>
+class MyFirstComponent extends React.Component{
+
+   render(){
+     return (
+         <div>
+           <h1> Hello , 2+2 is {2+2}</h1>
+           </div>
+     ) 
+     
+   }
+
+}
+export default MyFirstComponent;
